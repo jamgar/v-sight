10706eb1b41017eb1d5566064ded042a4b50066d
github username <github email address>
2019-04-07 10:03:13 -0500

    Create Orders table with mock data

--- src/main.js
+++ src/main.js
@@ -1,12 +1,20 @@
 import Vue from 'vue'
+import moment from 'moment'
+
 import App from './App.vue'
 import router from './router'
 import store from './store'
+
 import 'bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import '@/assets/css/g-styles.css'
 
 Vue.config.productionTip = false
+Vue.filter('formatDate', function(value) {
+  if (value) {
+    return moment(String(value)).format('MM/DD/YYYY hh:mm')
+  }
+})
 
 new Vue({
   router,
---
+++ src/views/SectionOrders.vue
@@ -0,0 +1,98 @@
+<template>
+  <div class="section-container">
+    <h2>Latest Orders</h2>
+    <table class="table table-inverse table-hover">
+      <thead class="thead-dark">
+        <tr>
+          <th>ID</th>
+          <th>Customer</th>
+          <th>Amount</th>
+          <th>Order Placed</th>
+          <th>Order Fulfilled</th>
+          <th>Status</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="order in orders">
+          <td>{{ order.id }}</td>
+          <td>{{ order.customer.name }}</td>
+          <td>{{ order.total }}</td>
+          <td>{{ order.placed | formatDate }}</td>
+          <td>{{ order.fulfilled | formatDate }}</td>
+          <td>Status</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    orders: [
+      {
+        id: 1,
+        customer: {
+          id: 1,
+          name: 'XYZ Logics',
+          state: 'FL',
+          email: 'sales@example.com'
+        },
+        total: 230,
+        placed: new Date(2017, 12, 1),
+        fulfilled: new Date(2017, 12, 3)
+      },
+      {
+        id: 2,
+        customer: {
+          id: 2,
+          name: 'XYZ Logics',
+          state: 'FL',
+          email: 'sales@example.com'
+        },
+        total: 230,
+        placed: new Date(2017, 12, 1),
+        fulfilled: new Date(2017, 12, 3)
+      },
+      {
+        id: 3,
+        customer: {
+          id: 3,
+          name: 'XYZ Logics',
+          state: 'FL',
+          email: 'sales@example.com'
+        },
+        total: 230,
+        placed: new Date(2017, 12, 1),
+        fulfilled: new Date(2017, 12, 3)
+      },
+      {
+        id: 4,
+        customer: {
+          id: 4,
+          name: 'XYZ Logics',
+          state: 'FL',
+          email: 'sales@example.com'
+        },
+        total: 230,
+        placed: new Date(2017, 12, 1),
+        fulfilled: new Date(2017, 12, 3)
+      },
+      {
+        id: 5,
+        customer: {
+          id: 5,
+          name: 'XYZ Logics',
+          state: 'FL',
+          email: 'sales@example.com'
+        },
+        total: 230,
+        placed: new Date(2017, 12, 1),
+        fulfilled: new Date(2017, 12, 3)
+      }
+    ]
+  })
+}
+</script>
+
+<style scoped></style>
