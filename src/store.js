1d40d98c1da8f8ac87ce6abebb699dee81940fb6
github username <github email address>
2019-07-27 08:40:11 -0500

    Add linechart data

--- src/store.js
+++ src/store.js
@@ -7,15 +7,23 @@ Vue.use(Vuex)
 export default new Vuex.Store({
   state: {
     orders: [],
+    ordersByCustomer: [],
+    ordersByState: [],
     total: 0,
     totalPages: 0,
     loading: false
   },
   mutations: {
-    UPDATE_ORDERS(state, payload) {
+    SET_ORDERS(state, payload) {
       state.orders = payload.data.page.data
       state.totalPages = payload.data.totalPages
       state.orders = payload.data.page.data
+    },
+    SET_ORDERS_BY_CUSTOMERS(state, payload) {
+      state.ordersByCustomer = payload.data
+    },
+    SET_ORDERS_BY_STATE(state, payload) {
+      state.ordersByState = payload
     },
     SET_LOADING(state, loading) {
       state.loading = loading
@@ -32,7 +40,27 @@ export default new Vuex.Store({
           }`
         )
         .then(response => {
-          commit('UPDATE_ORDERS', response)
+          commit('SET_ORDERS', response)
+          commit('SET_LOADING', false)
+        })
+    },
+    async getOrdersByCustomer({ commit }, number) {
+      commit('SET_LOADING', true)
+
+      await axios
+        .get(`https://localhost:5001/api/order/bycustomer/${number}`)
+        .then(response => {
+          commit('SET_ORDERS_BY_CUSTOMERS', response)
+          commit('SET_LOADING', false)
+        })
+    },
+    async getOrdersByState({ commit }, number = 6) {
+      commit('SET_LOADING', true)
+
+      await axios
+        .get(`https://localhost:5001/api/order/bystate/`)
+        .then(response => {
+          commit('SET_ORDERS_BY_STATE', response.data.slice(0, number))
           commit('SET_LOADING', false)
         })
     }
--- src/views/SectionSales.vue
+++ src/views/SectionSales.vue
@@ -7,11 +7,7 @@
             <h5>Daily Orders</h5>
           </div>
           <div class="card-body">
-            <BarChart
-              class="chart"
-              :chart-data="barDataCollection"
-              :options="options"
-            />
+            <BarChart class="chart" :chart-data="barDataCollection" :options="options"/>
           </div>
         </div>
         <div class="card mb-3 shadow card-theme">
@@ -19,11 +15,7 @@
             <h5>Orders by Customer</h5>
           </div>
           <div class="card-body">
-            <PieChart
-              class="chart"
-              :chart-data="pieDataCollection"
-              :options="options"
-            />
+            <PieChart class="chart" :chart-data="pieDataCollectionByCustomer" :options="options"/>
           </div>
         </div>
         <div class="card mb-3 shadow card-theme">
@@ -31,11 +23,7 @@
             <h5>Orders by State</h5>
           </div>
           <div class="card-body">
-            <PieChart
-              class="chart"
-              :chart-data="pieDataCollection"
-              :options="options"
-            />
+            <PieChart class="chart" :chart-data="pieDataCollectionByState" :options="options"/>
           </div>
         </div>
       </div>
@@ -61,6 +49,7 @@
 <script>
 import { mapState } from 'vuex'
 import moment from 'moment'
+import { THEME_COLORS } from '../utils/theme_colors'
 import BarChart from '@/components/charts/BarChart.vue'
 import PieChart from '@/components/charts/PieChart.vue'
 import LineChart from '@/components/charts/LineChart.vue'
@@ -73,8 +62,11 @@ export default {
   },
   data() {
     return {
+      themeColor: 'Bright',
+      topCustomers: [],
       barDataCollection: null,
-      pieDataCollection: null,
+      pieDataCollectionByCustomer: null,
+      pieDataCollectionByState: null,
       lineDataCollection: null,
       lineStyles: {
         height: '90%',
@@ -90,11 +82,24 @@ export default {
     }
   },
   computed: {
-    ...mapState(['orders'])
+    ...mapState(['orders', 'ordersByCustomer', 'ordersByState']),
+    themeColors() {
+      const colors = THEME_COLORS.slice(0).find(
+        set => set.name === this.themeColor
+      ).colorSet
+
+      return colors
+    }
   },
   mounted() {
-    this.$store.dispatch('getOrders', { page: 1, perPage: 10 }).then(() => {
+    this.$store.dispatch('getOrders', { page: 1, perPage: 100 }).then(() => {
       this.setBarChartData()
+    })
+    this.$store.dispatch('getOrdersByCustomer', 3).then(() => {
+      this.$store.dispatch('getOrdersByState').then(() => {
+        this.setPieChartData()
+        this.setLineChartData()
+      })
     })
   },
   methods: {
@@ -128,39 +133,57 @@ export default {
         ]
       }
     },
-    fillData() {
-      this.barDataCollection = {
-        labels: ['W1', 'W2', 'W3', 'W4', 'W5', 'W6', 'W7'],
+    setPieChartData() {
+      const colors = this.themeColors
+
+      this.pieDataCollectionByCustomer = {
+        labels: this.ordersByCustomer.map(o => o.name),
         datasets: [
           {
-            label: 'Q3 Sales',
-            backgroundColor: '#f87979',
-            data: [65, 59, 80, 81, 56, 54, 30]
-          },
-          {
-            label: 'Q4 Sales',
-            backgroundColor: '#A5CC82',
-            data: [25, 39, 60, 91, 36, 59, 80]
-          }
-        ]
-      }
-      this.pieDataCollection = {
-        labels: ['XYZ Logics', 'Spacely Sprockets', 'Cogwell Cog'],
-        datasets: [
-          {
-            label: 'Q3 Sales',
-            data: [165, 259, 380],
-            backgroundColor: ['#26547C', '#ff6b6b', '#ffd166'],
+            label: '',
+            data: this.ordersByCustomer.map(d => d.total),
+            backgroundColor: colors,
             borderColor: 'lightgreen'
           }
         ]
       }
-      this.lineDataCollection = {
-        labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun'],
+      this.pieDataCollectionByState = {
+        labels: this.ordersByState.map(o => o.state),
         datasets: [
           {
-            label: 'Sentiment Analysis',
-            data: [32, 14, 46, 23, 38, 54],
+            label: '',
+            data: this.ordersByState.map(d => d.total),
+            backgroundColor: colors,
+            borderColor: 'lightgreen'
+          }
+        ]
+      }
+    },
+    setLineChartData() {
+      this.topCustomers = this.ordersByCustomer.map(cx => cx['name'])
+      const allChartData = this.topCustomers.reduce((res, i) => {
+        res.push(this.getLineChartData(this.orders, i))
+        return res
+      }, [])
+
+      let dates = allChartData
+        .map(x => x['data'])
+        .reduce((a, i) => {
+          a.push(i.map(o => new Date(o[0])))
+          return a
+        }, [])
+
+      dates = [].concat.apply([], dates)
+
+      const ordersByDate = this.getCustomerOrdersByDate(allChartData, dates)[
+        'data'
+      ]
+      this.lineDataCollection = {
+        labels: ordersByDate[0]['orders'].map(o => o['date']),
+        datasets: [
+          {
+            data: ordersByDate[0]['orders'].map(x => x['total']),
+            label: ordersByDate[0]['customer'],
             backgroundColor: 'rgba(6, 214, 160, 0.2)',
             borderColor: 'rgba(0, 200, 140, 0.5)',
             pointBackgroundColor: '#000',
@@ -168,8 +191,8 @@ export default {
             pointHoverBorderColor: '#555'
           },
           {
-            label: 'Image Recognition',
-            data: [12, 18, 46, 13, 28, 44],
+            data: ordersByDate[1]['orders'].map(x => x['total']),
+            label: ordersByDate[1]['customer'],
             backgroundColor: 'rgba(255, 209, 102, 0.2)',
             borderColor: 'rgba(240, 180, 89, 0.5)',
             pointBackgroundColor: '#000',
@@ -177,8 +200,8 @@ export default {
             pointHoverBorderColor: '#555'
           },
           {
-            label: 'Forecasting',
-            data: [52, 34, 49, 53, 68, 62],
+            data: ordersByDate[2]['orders'].map(x => x['total']),
+            label: ordersByDate[2]['customer'],
             backgroundColor: 'rgba(15, 78, 133, 0.2)',
             borderColor: 'rgba(3, 64, 128, 0.5)',
             pointBackgroundColor: '#000',
@@ -187,6 +210,59 @@ export default {
           }
         ]
       }
+    },
+    getLineChartData(allOrders, name) {
+      const customerOrders = allOrders.filter(o => o.customer.name === name)
+
+      const formattedOrders = customerOrders.reduce((r, e) => {
+        r.push([e.placed, e.total])
+        return r
+      }, [])
+
+      const result = { customer: name, data: formattedOrders }
+      return result
+    },
+    getCustomerOrdersByDate(orders, dates) {
+      const customers = this.topCustomers
+      const prettyDates = dates.map(x => this.toFriendlyDate(x))
+      const uniqDates = Array.from(new Set(prettyDates)).sort()
+
+      const result = {}
+      const dataSets = (result['data'] = [])
+
+      customers.reduce((x, y, i) => {
+        const customerOrders = []
+        dataSets[i] = {
+          customer: y,
+          orders: uniqDates.reduce((r, e, j) => {
+            const obj = {}
+            obj['date'] = e
+            obj['total'] = this.getCustomerDateTotal(e, y) //sum total orders for this customer on this date
+            customerOrders.push(obj)
+            return customerOrders
+          })
+        }
+        return x
+      }, [])
+
+      return result
+    },
+    toFriendlyDate(date) {
+      return moment(date)
+        .endOf('day')
+        .format('YY-MM-DD')
+    },
+    getCustomerDateTotal(date, customer) {
+      const r = this.orders.filter(
+        o =>
+          o.customer.name === customer && this.toFriendlyDate(o.placed) === date
+      )
+
+      const result = r.reduce((a, b) => {
+        return a + b.total
+      }, 0)
+
+      return result
     }
   }
 }
